a0a9e94e150ab901e0793bd62aa3cb5316a11139
katieolson84 <katieolson84@gmail.com>
2021-03-09 16:05:48 -0700

    get comments is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -93,4 +93,20 @@ router.delete('/:id', async (req,res) => {
     }
 })
 
+//Get comments
+router.get('/:id/comments', (req,res) => {
+    Post.findPostComments(req.params.id)
+        .then(comments => {
+            if(comments.length > 0) {
+                res.status(200).json(comments);
+            }else{
+                res.status(404).json({message: 'The comments information could not be retrieved'})
+            }
+        })
+        .catch (err => {
+            console.log(err);
+            res.status(500).json({message: "The post with the specified ID does not exist", err})
+        })
+})
+
 module.exports = router
